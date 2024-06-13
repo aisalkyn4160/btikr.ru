52425258b7b890df7e7972709873e70c776b4b1f
aisalkyn4160 <aisalkyn.4160@gmail.com>
2024-06-14 01:58:54 +0600

    adding modal windows

--- script.js
+++ script.js
@@ -10,12 +10,79 @@ burger.addEventListener("click", () => {
         document.body.classList.remove("no-scroll");
     }
 })
+// ------------------------------------------------------------------------------------------------
+const serchBlock = document.querySelector(".search-block");
+const serchIcon = document.querySelector(".serch-icon");
+const searchInp = document.querySelector(".search-block input");
 
+if(window.innerWidth > 1010){
+    serchIcon.addEventListener("click", () => {
+        serchBlock.classList.toggle("active");
+        if(serchBlock.classList.contains("active")){
+            searchInp.focus();
+        }else{
+            searchInp.value = "";
+        }
+        
+    })
+}
 // --------------------------------------------------------------------------------------------------------------
+
+function ready(){
+    const inputs = document.querySelectorAll(".input-file");
+    Array.prototype.forEach.call(inputs, function(input){
+        const label = input.nextElementSibling;
+        const fileNameSpan = label.nextElementSibling;
+        let labelVal = fileNameSpan.innerHTML;
+
+        input.addEventListener("change", function(e){
+            let fileName = "";
+            if(this.files && this.files.length > 1)
+                fileName = (this.getAttribute("data-multiple-caption") || "").replace("{count}", this.files.length);
+            else
+                fileName = e.target.value.split("\\").pop();
+
+            if(fileName)
+                fileNameSpan.innerHTML = fileName;
+            else
+                fileNameSpan.innerHTML = labelVal;
+            
+    })
+})
+}
+
+ready();
+
+
+// ----------------------------------popup--------------------------------
+const popup = document.querySelector('.popup')
+const popupShowBtns = document.querySelectorAll('.show-popup')
+const closeFormBtn = document.querySelector('.close-form')
+
+popupShowBtns.forEach(item => {
+    item.addEventListener('click', () => {
+        popup.classList.add('show-popup')
+        document.body.classList.add('no-scroll')
+    })
+})
+
+closeFormBtn.addEventListener('click', () => {
+    popup.classList.remove('show-popup')
+    document.body.classList.remove('no-scroll')
+})
+
+document.addEventListener('click', (event) => {
+    if (event.target === popup) {
+        popup.classList.remove('show-popup')
+        document.body.classList.remove('no-scroll')
+    }
+});
+
+
+// ---------------------------------------------------------
 const link = document.querySelector("a[disabled]");
-const btn = link.querySelector("button");
-console.log(link)
+const btn = link.querySelector(".disabled-btn");
+
 if(btn){
     btn.disabled = true;
 }
-
